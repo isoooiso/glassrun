d798fd1cba459ce78429eca1df4ca3648d179b2e
rashid93 <101974133+isoooiso@users.noreply.github.com>
2026-01-21 21:10:34 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@ const nextConfig = {
   basePath: "/glassrun",
   assetPrefix: "/glassrun",
   trailingSlash: true,
+
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "pino-pretty": false,
+      "@react-native-async-storage/async-storage": false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
